59cf0ff5012a3fc5fda2cfea3acfcd4868afff9f
Ranj <ranjdotdev@gmail.com>
2025-04-13 14:37:50 +0300

    Extending the token set & the lexer

--- token/token.go
+++ token/token.go
@@ -16,6 +16,14 @@ const (
 	// Operators
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
 	// Delimiters
 	COMMA = ","
 	SEMICOLON = ";"
@@ -26,11 +34,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	VAR = "VAR"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType{
 	"fn": FUNCTION,
 	"var": VAR,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
