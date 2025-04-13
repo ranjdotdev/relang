1b35edf34a56983b9d7c29fcf237bcf8e444d4bf
Ranj <ranjdotdev@gmail.com>
2025-04-13 15:38:57 +0300

    Start of a REPL

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/ranjdotdev/relang/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil { panic(err) }
+	fmt.Printf("Hello %s! This is Relang.\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
